237fd501899c6a0b8bcf43472f88fb4d5c84dd49
Bharatdwaj3 <badrikedar.jaydutt.456@gmail.com>
2026-01-06 16:39:47 +0530

    added a baasic setup for backend

---
+++ backend/routes/abc.routes.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import { 
+  createUser, 
+  getAllUsers, 
+  getUserById, 
+  updateUser, 
+  deleteUser 
+} from '../controllers/userController.js';
+import validateUser from '../middleware/inputValidator.js';
+
+const router = express.Router();
+
+router.post('/', validateUser, createUser);
+router.get('/', getAllUsers);
+router.get('/:id', getUserById);
+router.put('/:id', validateUser, updateUser);
+router.delete('/:id', deleteUser);
+
+export default router;
